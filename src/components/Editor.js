6f47a41c3e8e5e74bb73adb20dcc1fcbf30035b2
SebTota <kidokyo1@gmail.com>
2021-07-19 21:10:35 -0400

    added base delegate and data retention functions

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Quill from 'quill';
+import FilesafeEmbed from 'filesafe-embed';
+import Filesafe from 'filesafe-js';
+import EditorKit from '@standardnotes/editor-kit';
 
 
 export default class Editor extends React.Component {
@@ -14,13 +17,46 @@ export default class Editor extends React.Component {
     }
 
     configureEditorKit() {
+        const delegate = {
+            insertRawText: (rawText) => {
+                let index = 0;
+                if (this.quill.getSelection()) {
+                    index = this.quill.getSelection().index
+                }
+                this.quill.clipboard.dangerouslyPasteHTML(index, rawText, 'api')
+            },
+            setEditorRawText: (rawText) => {
+                const quillDelta = this.quill.clipboard.convert(rawText)
+                this.quill.setContents(quillDelta)
+            },
+            getCurrentLineText: () => {
+            },
+            getPreviousLineText: () => {
+            },
+            replaceText: ({ regex, replacement, previousLine }) => {
+            },
+            clearUndoHistory: () => {
+                // Called when switching notes to prevent history mixup.
+                this.quill.history.clear()
+            }
+        };
 
+        this.editorKit = new EditorKit(delegate, {
+            mode: 'html',
+            supportsFileSafe: false,
+            coallesedSavingDelay: 0
+        });
     }
 
     async configureEditor() {
-        var editor = new Quill('#editor', {
+        this.quill = new Quill('#editor', {
             modules: { toolbar: '#toolbar' },
             theme: 'snow'
+        });
+
+        const c = this;
+        this.quill.on('text-change', function(delta, oldDelta, source) {
+            c.editorKit.onEditorValueChanged(c.quill.root.innerHTML);
         });
     }
 
